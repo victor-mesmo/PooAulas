b4a2d31e10b30692c16918d3074a6ecb8e08a7b5
victor-mesmo <rodrigues.victor@academico.ifrn.edu.br>
2016-05-24 16:45:51 -0300

    atualizaçao

---
+++ aula1.cpp
@@ -0,0 +1,26 @@
+// meu primerio codigo
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* args[]) 
+{
+	string jog1, jog2;
+	int vida = 20, vida2 = 20
+		
+	cout << "Digite o nome do jogador 1 " << endl;
+	cin >> jog1;
+
+	cout << "Digite o nome do jogador 2" << endl;
+	cin >> jog2
+	
+	cout << "O         O " << endl;
+	cout << ".T./       \\.T." << endl;
+	cout << "^       ^" << endl;
+	cout << jog1 << "       " << jog2 << endl;
+	cout << "vida: " <<"    " << "Vida: " << vida2 << endl;
+	
+	
+	return 0;
+
+}
